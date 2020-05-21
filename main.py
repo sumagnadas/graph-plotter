b766fbc562a8f9ef8d19d1fd5be629465aeb9127
sumagnadas <sumagnadas@gmail.com>
2020-05-21 17:10:25 +0530

    Added one more test

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ from modules import geometry, configureImage
 from modules.gui import Graph, QtWidgets
 import sys
 
-# Some necessary variables
-app = QtWidgets.QApplication(["Graph Plotter"])  # Name of the application
-rect = app.primaryScreen().availableGeometry()
-WIDTH = int(rect.width() * (43.92 / 100))
-HEIGHT = int(rect.height() * (80.97 / 100))  # Width and height of the graph
 
-# Configures the graph image according to the screen size
-configureImage(WIDTH, HEIGHT)
+
 
 # The code for the main application
-if __name__ == "__main__":
+def main():
+    # Some necessary variables
+    app = QtWidgets.QApplication(["Graph Plotter"])  # Name of the application
+    rect = app.primaryScreen().availableGeometry()
+    WIDTH = int(rect.width() * (43.92 / 100))
+    HEIGHT = int(rect.height() * (80.97 / 100))  # Width and height of the graph
+
+    # Configures the graph image according to the screen size
+    configureImage(WIDTH, HEIGHT)
+
     graphPlotter = Graph()  # Make the window
     graphPlotter.show()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
--- tests/test_misc.py
+++ tests/test_misc.py
@@ -10,3 +10,11 @@ def test_extrainfo(qtbot):
     qtbot.addWidget(window)
     qtbot.mouseClick(window.extraInfo, Qt.LeftButton)
     assert window.shapeInfo.isHidden() == False, 'Extra Info is not being shown'
+
+def test_openfigure(qtbot):
+    qtbot.addWidget(window)
+    qtbot.mouseClick(window.addButton, Qt.LeftButton)
+    window.buttonBox.buttons()[3].setCheckState(Qt.CheckState.Unchecked)
+    assert window.shapeName.text() == 'Angle', 'Open figures are not being plotted(BUG)'
+    window.buttonBox.buttons()[3].setCheckState(Qt.CheckState.Checked)
+    assert window.shapeName.text() == 'Triangle', 'Closed figures are not being plotted just after drawing an open figure(BUG)'
