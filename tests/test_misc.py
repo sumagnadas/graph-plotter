60976fa234aed501cd2f17e477a7ddd339d302de
sumagnadas <sumagnadas@gmail.com>
2020-06-16 20:37:07 +0530

    Fixed the build

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -3,9 +3,10 @@ from modules.gui import QtGui, Graph, QtWidgets
 from PySide2.QtCore import Qt
 from os import path
 
-h = QtWidgets.QApplication.instance()
-window = Graph()
+
 def test_closedfigure(qtbot):
+    h = QtWidgets.QApplication.instance()
+    window = Graph()
     qtbot.addWidget(window)
     qtbot.mouseClick(window.addButton, Qt.LeftButton)
     window.buttonBox.buttons()[4].setCheckState(Qt.CheckState.Unchecked)
@@ -15,17 +16,23 @@ def test_closedfigure(qtbot):
     qtbot.mouseClick(window.input[-1].removeButton, Qt.LeftButton)
 
 def test_extrainfo(qtbot):
+    h = QtWidgets.QApplication.instance()
+    window = Graph()
     qtbot.addWidget(window)
     qtbot.mouseClick(window.extraInfo, Qt.LeftButton)
     assert window.shapeInfo.isHidden() == False, 'Extra Info is not being shown'
 
 def test_removebutton(qtbot):
+    h = QtWidgets.QApplication.instance()
+    window = Graph()
     oldRowCount = window.coordLayout.rowCount()
     qtbot.mouseClick(window.addButton, Qt.LeftButton)
     qtbot.mouseClick(window.input[-1].removeButton, Qt.LeftButton)
     assert window.coordLayout.rowCount() == oldRowCount, "\"Remove Point\" button is not working"
 
 def test_clear(qtbot):
+    h = QtWidgets.QApplication.instance()
+    window = Graph()
     window.x_coord1.setText('10')
     window.y_coord1.setText('10')
     window.x_coord2.setText('20')
@@ -39,5 +46,7 @@ def test_clear(qtbot):
     assert window.y_coord2.text() == '0', "Reset button is not working"
 
 def test_addbutton(qtbot):
+    h = QtWidgets.QApplication.instance()
+    window = Graph()
     qtbot.mouseClick(window.addButton, Qt.LeftButton)
     assert window.coordLayout.rowCount() == 4, "\"Add Point\" button is not working"
